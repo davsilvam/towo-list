ab336da996fdc4c891489a209558dee98ea553df
David Silva <contatodavidmaia@gmail.com>
2022-12-08 15:34:36 -0300

    :lipstick: feat: ajustes no background do card de aviso de sem exercícios

--- src/components/NoExercises.vue
+++ src/components/NoExercises.vue
@@ -5,8 +5,7 @@
 <template>
   <div class="w-full flex-center">
     <div
-      id="card"
-      class="w-fit flex-between items-start gap-4 px-8 py-4 rounded-lg"
+      class="w-fit flex-between items-start gap-4 px-8 py-4 rounded-lg bg-neutral-900"
     >
       <ExclamationCircleIcon class="w-6 text-yellow-500 pt-1" />
       <div class="w-60 lg:w-96">
@@ -18,13 +17,3 @@
     </div>
   </div>
 </template>
-
-<style scoped>
-  #card {
-    background: rgba(38, 38, 38, 0.65);
-    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-    backdrop-filter: blur(8.1px);
-    -webkit-backdrop-filter: blur(8.1px);
-    border: 1px solid rgba(38, 38, 38, 0.21);
-  }
-</style>
